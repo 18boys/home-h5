b9edcd2672a9fc531fb626180a24c98dd86472bb
shuai.li12 <shuai.li12@bianlifeng.com>
2018-02-08 21:11:11 +0800

    feat 1.完成基本大致 ui

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,10 +2,10 @@
 var FastClick = require('libs/fastclick');
 // require('./libs/zeptofullpage');
 // require('./libs/jsfullpage.js');
-
+var enableInlineVideo = require('libs/iphone-inline-video');
 FastClick(document.body);
 
-
+console.log('enableInlineVideo', enableInlineVideo);
 var screenWidth = document.body.clientWidth,
   screenHeight = document.body.clientHeight,
   originWidth = 375,
@@ -13,6 +13,9 @@ var screenWidth = document.body.clientWidth,
 
 var Page = function () {
   this.$body = $('.pageWrapper');
+  this.$home = $('.home');
+  this.$full_page = $('.full-page');
+  this.$share_page = $('.share-page');
   this.init();
 };
 
@@ -20,6 +23,8 @@ var Page = function () {
 Page.prototype = {
   init: function () {
     this._reset();
+    this._initHomePage();
+    this._initEvent();
     this._run();
   },
   _reset: function () {
@@ -27,6 +32,32 @@ Page.prototype = {
       '-webkit-transform': 'scaleX(' + screenWidth / originWidth + ') scaleY(' + screenHeight / originHeight + ')',
       transform: 'scaleX(' + screenWidth / originWidth + ') scaleY(' + screenHeight / originHeight + ')'
     });
+  },
+  _initEvent: function () {
+    var _this = this;
+    $(document).on('click', '.home .play-icon', function (e) {
+      // 隐藏掉 home
+      _this.$home.addClass('hide');
+      // 显示出来全屏播放页面
+      _this.$full_page.addClass('on').removeClass('hide');
+      var full_video = document.getElementById("full-vidio");
+      // 开始播放
+      full_video.play();
+      $(full_video).on('ended', function (e) {
+        _this._initSharePage();
+        _this.$full_page.addClass('hide');
+      })
+    })
+  },
+  _initSharePage() {
+    this.$share_page.addClass('on').removeClass('hide');
+  },
+  _initHomePage() {
+    var bgAudio = document.getElementById('pre-vidio');
+    enableInlineVideo(bgAudio);
+    document.addEventListener("WeixinJSBridgeReady", function () {
+      bgAudio.play();
+    }, false);
   },
   _run: function () {
   }
